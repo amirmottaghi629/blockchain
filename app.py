57e0f5b575aa423c24d03e91ab50ed94a7ff5abf
amirmottaghe <amirmottaghe629@gmail.com>
2024-03-12 16:09:12 +0330

    fix jinga pro

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask,render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template('siteflask.html')
+    return render_template('pageone.html')
 if __name__=='__main__':
     app.run(debug=True)
+    
