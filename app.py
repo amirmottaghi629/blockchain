1016db48166137b25134e4d3b3cb63ee8d990c30
amirmottaghe <amirmottaghe629@gmail.com>
2024-03-16 16:33:36 +0330

    add login page

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from flask import Flask,render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template('pageone.html')
+    return render_template('home page.html')
+@app.route('/login_page.html')
+def login():
+    return render_template('login_page.html')
+@app.route('/contact_us.html')
+def contact():
+    return render_template('contact_us.html')
 if __name__=='__main__':
     app.run(debug=True)
