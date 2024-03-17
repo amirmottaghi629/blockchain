5c357149fdeacbf00a0e5865082af7a718beacb7
amirmottaghe <amirmottaghe629@gmail.com>
2024-03-17 17:40:19 +0330

    fix bug

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ from flask import Flask,render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
-<<<<<<< HEAD
-    return render_template('index.html')
-=======
     return render_template('home page.html')
 @app.route('/login_page.html')
 def login():
@@ -13,6 +10,5 @@ def login():
 @app.route('/contact_us.html')
 def contact():
     return render_template('contact_us.html')
->>>>>>> 1016db48166137b25134e4d3b3cb63ee8d990c30
 if __name__=='__main__':
     app.run(debug=True)
